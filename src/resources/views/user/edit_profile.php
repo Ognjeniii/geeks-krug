f627452d205eaa01c3de5b3521c461d1cc7868b5
ognjeniii <ilicognjen02@gmail.com>
2023-10-15 16:02:23 +0200

    created edit profile

--- src/resources/views/user/edit_profile.php
+++ src/resources/views/user/edit_profile.php
@@ -16,6 +16,81 @@
 </head>
 <body>
 
+<div class="container">
+    <form action="">
+
+        <div class="mb-3">
+            <label for="full_name" class="form-label">Full name:</label>
+            <input type="text" name="full_name" value="abc">
+        </div>
+
+        <div class="mb-3">
+            <label for="username" class="form-label">Username:</label>
+            <input type="text" name="username" value="abc">
+        </div>
+
+        <div class="mb-3">
+            <label for="profile_picture" class="form-label">Profile picture:</label>
+            <input type="image" name="profile_picture" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="phone_number" class="form-label">Phone number:</label>
+            <input type="tel" name="phone_number" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="address" class="form-label">Address:</label>
+            <input type="text" name="address" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="birthday" class="form-label">Birthday:</label>
+            <input type="date" name="birthday" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="education" class="form-label">Education:</label>
+            <input type="text" name="education" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="work_experience" class="form-label">Work experience:</label>
+            <input type="text" name="work_experience" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="programming_languages" class="form-label">Programming languages:</label>
+            <input type="text" name="programming_languages" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="github" class="form-label">GitHub:</label>
+            <input type="text" name="github" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="linkedin" class="form-label">LinkedIn:</label>
+            <input type="text" name="linkedin" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="x" class="form-label">X:</label>
+            <input type="text" name="x" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="leetcode" class="form-label">LeetCode:</label>
+            <input type="text" name="leetcode" value="abc" class="form-text">
+        </div>
+
+        <div class="mb-3">
+            <label for="website" class="form-label">Website:</label>
+            <input type="text" name="website" value="abc" class="form-text">
+        </div>
+
+    </form>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
