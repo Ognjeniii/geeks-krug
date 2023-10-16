6bd9536f3a93967d59cefa0fdf6f4b3fb291d1f5
bodafoo <140969236+bodafoo@users.noreply.github.com>
2023-10-16 19:41:04 +0200

    added forms for every edit profile field

--- src/resources/views/user/edit_profile.php
+++ src/resources/views/user/edit_profile.php
@@ -37,152 +37,286 @@ $user = User::getUserById($user_id);
     </div>
 
     <div class="container bg-dark-subtle p-4 rounded-2 shadow-sm" style="margin-top: -37px;">
-        <form action="../../../User/logic/edit_profile.php">
 
-            <p class="fs-5">Account Information</p>
+        <p class="fs-5">Account Information</p>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="username-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="username" class="form-label mb-0">Username:</label>
                     <input type="text" class="form-control" name="username" placeholder="<?php echo $user->getUsername(); ?>" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="username-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="email-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="email" class="form-label mb-0">Email:</label>
                     <input type="email" class="form-control" name="email" placeholder="<?php echo $user->getEmail(); ?>" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="email-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3">
-                <label for="email" class="form-label">Password:</label>
-                <a href="#" class="nav-item nav-link text-primary d-inline-block">Change Password</a>
-            </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password:</label>
+            <a href="#" class="nav-item nav-link text-primary d-inline-block">Change Password</a>
+        </div>
 
-            <p class="fs-5">Basic Info</p>
+        <hr />
+        <p class="fs-5">Basic Info</p>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="fullname-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="full_name" class="form-label mb-0">Full Name:</label>
                     <input type="text" class="form-control" name="full_name" placeholder="<?php echo $user->getFullName(); ?>" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="fullname-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <!-- we need gender in db -->
+        <!-- we need gender in db -->
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="gender-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="gender" class="form-label mb-0">Gender:</label>
-                    <select name="gender" class="form-control" disabled>
+                    <select id="gender-select" name="gender" class="form-control" disabled>
                         <option value="select">Select...</option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
                     </select>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="gender-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="address-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="address" class="form-label mb-0">Address:</label>
                     <input type="text" class="form-control" name="address" placeholder="Serbia, Belgrade" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="address-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="birthday-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="birthday" class="form-label mb-0">Birthday:</label>
                     <input type="date" class="form-control" name="birthday" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="birthday-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="phonenumber-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="phone_number" class="form-label mb-0">Phone Number:</label>
                     <input type="tel" class="form-control" name="phone_number" placeholder="+38160123456" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="phonenumber-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="website-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="website" class="form-label mb-0">Website:</label>
                     <input type="text" class="form-control" name="website" placeholder="boda.foo" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="website-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="github-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="github" class="form-label mb-0">GitHub:</label>
                     <input type="text" class="form-control" name="github" placeholder="github.com/bodafoo" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="github-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="linkedin-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="linkedin" class="form-label mb-0">LinkedIn:</label>
                     <input type="text" class="form-control" name="linkedin" placeholder="linkedin.com/in/slobodan-zivanovic" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="linkedin-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="x-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="x" class="form-label mb-0">X:</label>
                     <input type="text" class="form-control" name="x" placeholder="twitter.com/twitter" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="x-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="leetcode-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="leetcode" class="form-label mb-0">LeetCode:</label>
                     <input type="text" class="form-control" name="leetcode" placeholder="bodafoo" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="leetcode-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <p class="fs-5">Experience</p>
+        <hr />
+        <p class="fs-5">Experience</p>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="workexperience-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="work_experience" class="form-label mb-0">Work Experience:</label>
                     <input type="text" class="form-control" name="work_experience" placeholder="something" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="workexperience-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="education-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="education" class="form-label mb-0">Education:</label>
                     <input type="text" class="form-control" name="education" placeholder="FON" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="education-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
+        </form>
 
-            <p class="fs-5">Skills</p>
+        <hr />
+        <p class="fs-5">Skills</p>
 
-            <div class="mb-3 d-flex justify-content-between align-items-center">
+        <form action="#" method="post">
+            <div class="mb-3 d-flex justify-content-between align-items-center" id="programminglanguages-row">
                 <div class="d-flex align-items-center gap-2">
                     <label for="programming_languages" class="form-label mb-0">Programming Languages:</label>
                     <input type="text" class="form-control" name="programming_languages" placeholder="C, CPP" disabled>
                 </div>
-                <div><a href="#">Edit</a></div>
+                <div>
+                    <button type="button" class="edit-button btn btn-primary" data-row-id="programminglanguages-row">Edit</button>
+                    <button type="submit" class="save-button btn btn-success" style="display: none;">Save</button>
+                </div>
             </div>
-
         </form>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
+
+    <script>
+        function handleEditClick(event) {
+            event.preventDefault();
+            const editButton = event.target;
+            const rowId = editButton.getAttribute('data-row-id');
+            if (rowId) {
+                const row = document.getElementById(rowId);
+                const inputs = row.getElementsByTagName('input');
+                const select = row.querySelector('select');
+                if (select) {
+                    select.removeAttribute('disabled');
+                }
+                for (const input of inputs) {
+                    input.removeAttribute('disabled');
+                }
+
+                const saveButton = row.querySelector('.save-button');
+                const editButton = row.querySelector('.edit-button');
+
+                saveButton.style.display = 'block';
+                editButton.style.display = 'none';
+            }
+        }
+
+        function handleSaveClick(event) {
+            event.preventDefault();
+            const saveButton = event.target;
+            const row = saveButton.closest('div[id]');
+            const form = row.closest('form');
+            const inputs = row.getElementsByTagName('input');
+            const select = row.querySelector('select');
+            if (select) {
+                select.setAttribute('disabled', 'disabled');
+            }
+            for (const input of inputs) {
+                input.setAttribute('disabled', 'disabled');
+            }
+
+            const editButton = row.querySelector('.edit-button');
+
+            saveButton.style.display = 'none';
+            editButton.style.display = 'block';
+
+            form.submit();
+        }
+
+        const editButtons = document.querySelectorAll('.edit-button');
+        for (const button of editButtons) {
+            button.addEventListener('click', handleEditClick);
+        }
+
+        const saveButtons = document.querySelectorAll('.save-button');
+        for (const button of saveButtons) {
+            button.addEventListener('click', handleSaveClick);
+        }
+    </script>
+
 </body>
 
 </html>
