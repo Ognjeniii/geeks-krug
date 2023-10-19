85acc279abff612b868d4a9930e032955a6d163e
ognjeniii <ilicognjen02@gmail.com>
2023-10-19 22:57:38 +0200

    fix

--- src/User/logic/edit_profile/skills_info/add_programming_language.php
+++ src/User/logic/edit_profile/skills_info/add_programming_language.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/../../../../ProgrammingLanguage/ProgrammingLanguage.php';
-require __DIR__ . '/../../../../UserHelperClasses/UserProgrammingLanguages.php';
+require __DIR__ . '/../../../../UserProgrammingLanguages/UserProgrammingLanguages.php';
 
 session_start();
 $user_id = $_SESSION['user_id'];
